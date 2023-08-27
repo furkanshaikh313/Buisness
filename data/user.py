7fb6546f0ac5ae9b4933710b1b342f1b844d6626
meet447 <51074036+meet447@users.noreply.github.com>
2023-08-27 16:10:31 +0530

    backend for data hosting

--- data/user.py
+++ data/user.py
@@ -1,22 +1,30 @@
 import json 
 import random
+import requests
+
+base_url = "https://backendbuisness.meetsonawane.repl.co"  # Replace with your actual Flask app's URL
 
 def create_user(email, passw):
-   with open("users.json",'a') as file:
-       
-       data = {
-           "email":email,
-           "password":passw
-       }
-       
-       json.dump(data, file)
-       file.write("\n")
+    user_data = {
+        "email":email,
+        "password": passw
+    }
+
+    # Send a POST request to create a user
+    response = requests.post(f"{base_url}/create_users", json=user_data)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        print("User created successfully")
+    else:
+        print("Error:", response.text)
        
 def fetch_user():
-    posts = []
-    with open('users.json', 'r') as file:
-        for line in file:
-            post = json.loads(line)
-            posts.append(post)
-            
-    return posts
+    response = requests.get(f"{base_url}/fetch_users")
+
+    if response.status_code == 200:
+        users = response.json()
+        return users
+    
+    else:
+        print("Error:", response.text)
--- test.py
+++ test.py
@@ -1,5 +1,30 @@
-from data.user import create_user, fetch_user
+import requests
 
-create_user(email="sss", passw="dd")
+# Define the base URL for your Flask app
+base_url = "https://backendbuisness.meetsonawane.repl.co"  # Replace with your actual Flask app's URL
 
-users = print(fetch_user())
+# Example data for creating a user
+user_data = {
+    "email": "newusssser@example.com",
+    "password": "newpasswosssrd"
+}
+
+# Send a POST request to create a user
+response = requests.post(f"{base_url}/create_users", json=user_data)
+
+# Check if the request was successful
+if response.status_code == 200:
+    print("User created successfully")
+else:
+    print("Error:", response.text)
+
+# Send a GET request to fetch users
+response = requests.get(f"{base_url}/fetch_users")
+
+# Check if the request was successful
+if response.status_code == 200:
+    users = response.json()
+    print(users)
+  
+else:
+    print("Error:", response.text)
